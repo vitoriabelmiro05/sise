0cd3cab6b8c741fe7ed3c4823caddaa287606215
Igor Lamoia Queiroz <62469164+igorlamoia@users.noreply.github.com>
2021-03-21 19:22:57 -0300

    teste

--- painel.php
+++ painel.php
@@ -1,7 +1,8 @@
 <?php
 session_start();
 include('verifica_login.php');
-
+//Que atire a primeira pedra, aquele q nunca pecou
+//sjioadasjisdaokasdodsa
 ?>
 <h2>Olá, <?php echo  $_SESSION['CPF']; ?></h2>
 <h2><a href="logout.php">sair</a></h2>
